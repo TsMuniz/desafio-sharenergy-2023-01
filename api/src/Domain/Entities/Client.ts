26a24f82971c3cbd4a31bc85d7ffd4880cd0a606
thiago <ts_muniz@outlook.com>
2023-01-05 18:14:25 -0300

    feat: adiciona o campo _id do mongoDb a interface de user e client

--- api/src/Domain/Entities/Client.ts
+++ api/src/Domain/Entities/Client.ts
@@ -1,25 +1,28 @@
+import { ObjectId } from 'mongodb'
+
 export interface IClient {
-    name: string
-    email: string
-    address: string
-    phoneNumber: string
-    cpf: string
+  id?: string
+  _id?: ObjectId
+  name: string
+  email: string
+  address: string
+  phoneNumber: string
+  cpf: string
 }
 
-
 export class Client {
-    name: string;
-    email: string;
-    address: string;
-    phoneNumber: string;
-    cpf: string;
+  name: string
+  email: string
+  address: string
+  phoneNumber: string
+  cpf: string
 
-    constructor(client: IClient) {
-        const {name, email,address, phoneNumber, cpf} = client
-        this.name = name;
-        this.email = email;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.cpf = cpf;
-    }
+  constructor (client: IClient) {
+    const { name, email, address, phoneNumber, cpf } = client
+    this.name = name
+    this.email = email
+    this.address = address
+    this.phoneNumber = phoneNumber
+    this.cpf = cpf
+  }
 }
--- api/src/Domain/Entities/User.ts
+++ api/src/Domain/Entities/User.ts
@@ -1,18 +1,22 @@
-interface IUser {
-    name: string
-    email: string
-    password: string
+import { ObjectId } from 'mongodb'
+
+export interface IUser {
+  id?: string
+  _id?: ObjectId
+  name: string
+  email: string
+  password: string
 }
 
 export class User {
-    name: string;
-    email: string;
-    password: string
+  name: string
+  email: string
+  password: string
 
-    constructor(client: IUser) {
-        const {name, email,password} = client
-        this.name = name;
-        this.email = email;
-        this.password = password
-    }
+  constructor (client: IUser) {
+    const { name, email, password } = client
+    this.name = name
+    this.email = email
+    this.password = password
+  }
 }
