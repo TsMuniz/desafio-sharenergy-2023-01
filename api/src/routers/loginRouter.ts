47e06f1874cfb9f5254f7ff077cd6cefa977d2c5
thiago <ts_muniz@outlook.com>
2023-01-05 18:06:54 -0300

    feat: cria o arquivo de router para o login

---
+++ api/src/routers/loginRouter.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+import { Login } from '../Aplication/Login'
+import { MongoConnection } from '../database/mongoConnection'
+import { UserRepository } from '../repository/userRepository'
+
+export const loginRouter = Router()
+const mongoClient = new MongoConnection().getClient()
+const repository = new UserRepository('ShareEnergy', mongoClient)
+const login = new Login(repository)
+
+loginRouter.post('/', login.execute.bind(login))
+loginRouter.put('/access')
+loginRouter.put('/changeAccess')
