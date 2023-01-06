91e14929f7dcbe9843475fe178317f4adbd3355b
thiago <ts_muniz@outlook.com>
2023-01-06 08:34:17 -0300

    feat: adiciona validação de campos e encriptação de senha a classe CreateUser

--- api/src/Aplication/createUser.ts
+++ api/src/Aplication/createUser.ts
@@ -1,13 +1,33 @@
 import { User } from '../Domain/Entities/User'
 import { IUserRepository } from '../Domain/Repository/userRepository'
+import bcrypt from 'bcrypt'
+import Joi from 'joi'
+import CustomError from '../helpers/customError'
 
 export class CreateUser {
   constructor (readonly userRepository: IUserRepository) {
   }
 
+  private encryptPassword (password: string): string {
+    const salt = bcrypt.genSaltSync(10)
+    return bcrypt.hashSync(password, salt)
+  };
+
+  private async validate (name: string, password: string): Promise<Object | Error> {
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      password: Joi.string().min(8).required()
+    })
+    const { error, value } = schema.validate({ name, password })
+    if (error) throw new CustomError(error.message, 400)
+    return value
+  }
+
   async execute (input: User): Promise<void> {
-    const { name, email, password } = input
-    const user = new User({ name, email, password })
+    const { name, password } = input
+    await this.validate(name, password)
+    const encryptedPassword = this.encryptPassword(password)
+    const user = new User({ name, password: encryptedPassword })
     await this.userRepository.create(user)
   }
 }
