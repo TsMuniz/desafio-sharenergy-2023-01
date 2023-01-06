fed54580934eb3286bbcdb1ad6340297a3b2822e
thiago <ts_muniz@outlook.com>
2023-01-06 12:34:22 -0300

    feat: instancia a classe de busca de todos os clientes para cirar o endpoint findAll

--- api/src/routers/clientRouter.ts
+++ api/src/routers/clientRouter.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { CreateClient } from '../Aplication/createClient'
+import { FindAllClients } from '../Aplication/findAllClients'
 import { FindClient } from '../Aplication/findClient'
 import { MongoConnection } from '../database/mongoConnection'
 import { ClienteRepository } from '../repository/clientRepository'
@@ -9,6 +10,8 @@ const mongoClient = new MongoConnection().getClient()
 const clientRepository = new ClienteRepository('ShareEnergyDB', mongoClient)
 const createClient = new CreateClient(clientRepository)
 const findClient = new FindClient(clientRepository)
+const findAllClients = new FindAllClients(clientRepository)
 
 clientRouter.post('/create', createClient.execute.bind(createClient))
 clientRouter.get('/find/:id', findClient.execute.bind(findClient))
+clientRouter.get('/findAll', findAllClients.execute.bind(findAllClients))
