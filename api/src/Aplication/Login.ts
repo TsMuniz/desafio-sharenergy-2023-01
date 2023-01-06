2f5e508caa4e4273e2a33df6440698b729f231e3
thiago <ts_muniz@outlook.com>
2023-01-06 08:35:20 -0300

    feat: adiciona validação de campos e desencriptação de senha a classe Login

--- api/src/Aplication/Login.ts
+++ api/src/Aplication/Login.ts
@@ -1,11 +1,11 @@
-/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { IUser } from '../Domain/Entities/User'
 import { IUserRepository } from '../Domain/Repository/userRepository'
 import 'dotenv/config'
 import { sign } from 'jsonwebtoken'
 import { Request, Response } from 'express'
 import CustomError from '../helpers/customError'
+import Joi from 'joi'
+import bcrypt from 'bcrypt'
 
 const secret: string = process.env.JWT_SECRET || 'fgdfgsdfghsh3132154654'
 
@@ -13,24 +13,39 @@ export class Login {
   constructor (readonly userRepository: IUserRepository) {
   }
 
-  private async getUserByEmail (email: string): Promise<IUser> {
-    const user = await this.userRepository.readByEmail(email)
+  private async getUserByName (name: string): Promise<IUser> {
+    const user = await this.userRepository.readByName(name)
     return user
   }
 
   private async createToken (user: IUser): Promise<string> {
-    const { password, email } = user
-    const payload = { data: { email, password } }
+    const { password, name } = user
+    const payload = { data: { name, password } }
     const token = sign(payload, secret)
     return token
   }
 
-  private async checkEmailAndPassword (email: string, password: string): Promise<string | Error> {
-    const user = await this.getUserByEmail(email)
-    const { email: userEmail, password: userPassword } = user
+  private async validate (name: string, password: string): Promise<Object | Error> {
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      password: Joi.string().min(8).required()
+    })
+    const { error, value } = schema.validate({ name, password })
+    console.log(error)
+    if (error) throw new CustomError(error.message, 400)
+    return value
+  }
+
+  private decryptPassword (password: string, encryptedPassword: string): boolean {
+    return bcrypt.compareSync(password, encryptedPassword)
+  };
+
+  private async checkNameAndPassword (name: string, password: string): Promise<string | Error> {
+    const user = await this.getUserByName(name)
+    const { name: username, password: encryptedPassword } = user
     if (
-      userEmail === email &&
-      password === userPassword
+      username === name &&
+      this.decryptPassword(password, encryptedPassword)
     ) {
       return await this.createToken(user)
     }
@@ -38,8 +53,9 @@ export class Login {
   }
 
   public async execute (req: Request, res: Response): Promise<void> {
-    const { email, password } = req.body
-    const token = await this.checkEmailAndPassword(email, password)
+    const { name, password } = req.body
+    await this.validate(name, password)
+    const token = await this.checkNameAndPassword(name, password)
     res.status(200).json({ token })
   }
 }
